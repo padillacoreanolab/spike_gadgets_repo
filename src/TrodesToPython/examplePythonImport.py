0e67ef254c398abcd30f92e67b4a23c76ef36979
ryoiwata <ryoiwata@github.com>
2022-09-13 20:26:23 -0400

    refactor: adding underscore to functions and rename methods

--- src/TrodesToPython/examplePythonImport.py
+++
@@ -1,3 +0,0 @@
-import readTrodesExtractedDataFile3 as trodesReader
-data = trodesReader.readTrodesExtractedDataFile('/home/spikegadgets/datadump/dioexport/dioexport.DIO/dioexport.dio_ECU_Dout1.dat')
-print(data['data'])
